d5c84fd2a6956a5d6645b3b6e379a1db00ab1615
Jonas Schubert Erlandsson <jonas.schubert.erlandsson@gmail.com>
2018-11-26 12:08:14 +0100

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181126105444) do
+ActiveRecord::Schema.define(version: 20181126110747) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20181126105444) do
     t.integer  "todo_list_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.datetime "completed_at"
     t.index ["todo_list_id"], name: "index_todo_items_on_todo_list_id", using: :btree
   end
 
