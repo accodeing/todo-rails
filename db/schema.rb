0a996440c98d182b0739878578830ffd6e83cc68
Jonas Schubert Erlandsson <jonas.schubert.erlandsson@gmail.com>
2018-11-26 11:55:15 +0100

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181126105153) do
+ActiveRecord::Schema.define(version: 20181126105444) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "todo_items", force: :cascade do |t|
+    t.string   "content"
+    t.integer  "todo_list_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["todo_list_id"], name: "index_todo_items_on_todo_list_id", using: :btree
+  end
 
   create_table "todo_lists", force: :cascade do |t|
     t.string   "title"
@@ -22,4 +30,5 @@ ActiveRecord::Schema.define(version: 20181126105153) do
     t.datetime "updated_at",  null: false
   end
 
+  add_foreign_key "todo_items", "todo_lists"
 end
